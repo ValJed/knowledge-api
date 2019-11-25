0fdd4f302b6c22cb1206dc3e42f18b5e79004d16
Val <valentin.jeudy@gmail.com>
2019-11-25 19:50:56 +0100

    finally move my ddd structure to src, replacing old api

--- src/modules/index.js
+++
@@ -1,9 +0,0 @@
-const UsersRoutes = require('./Users')
-const blocksRoutes = require('./Blocks')
-const projetsRoutes = require('./Projects')
-
-module.exports = (app) => {
-  UsersRoutes(app)
-  projetsRoutes(app)
-  blocksRoutes(app)
-}
